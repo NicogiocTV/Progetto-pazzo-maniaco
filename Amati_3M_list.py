a1b964f655139e28172fedda601abe936a48040e
NicogiocTV <121234439+NicogiocTV@users.noreply.github.com>
2023-01-18 00:04:07 +0100

    Delete Amati_3M_list.py

--- Amati_3M_list.py
+++
@@ -1,6 +0,0 @@
-cognomi_list = ["Amati", "Andruccioli","arcolin", "Baffert" , "Chiarabini", "Ciaramidaro", "Cornacchia", "D'elia", "Del bianco", "Fanci", "Gibertini"]
-for i in cognomi_list:
-    print(i)
-
-cognomi_list.append ("Tonelli")
-print(cognomi_list)
