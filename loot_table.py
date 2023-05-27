b39e0774715ecdb86580dfea1277d6539497b17a
NicogiocTV <121234439+NicogiocTV@users.noreply.github.com>
2023-05-27 19:52:00 +0200

    Add files via upload

--- loot_table.py
+++ loot_table.py
@@ -1,8 +1,8 @@
 import random
 
-def loot():
-    loot_list = ["pozione", "super pozione", "iper pozione","pozione max", "breadboard"]
-    probability = [40 , 27.5 , 20  , 12.5 , 0.000000000000000000001] 
+def loot_ita():                 # una funzione con gli oggetti in italiano
+    loot_list = ["pozione", "super pozione", "iper pozione","pozione max", "breadboard"]    # oggetti che si possono trovare dopo ogni scontro
+    probability = [40 , 27.5 , 20  , 12.5 , 0.000000000000000000001]                        # probabilità di drop di ogni oggetto
 
     loot_probability = dict(zip(loot_list, probability))
     loot_prob=True
@@ -15,5 +15,16 @@ def loot():
         return loot_drop, index
     
 
+def loot_eng():                 # una funzione con gli oggetti in inglese
+    loot_list = ["potion", "super potion", "hyper potion","max potion","breadboard"]    # oggetti che si possono trovare dopo ogni scontro
+    probability = [40 , 27.5 , 20  , 12.5 , 0.000000000000000000001]                        # probabilità di drop di ogni oggetto
 
-        
+    loot_probability = dict(zip(loot_list, probability))
+    loot_prob=True
+    open_chest = "si"
+    if open_chest.lower() == "si" or open_chest.lower() == "s":
+        loot_drop = random.choices(loot_list, weights=probability)
+        loot_name = loot_drop[0]
+        index = loot_list.index(loot_name)
+        loot_prob = loot_probability[loot_name]
+        return loot_drop, index    
